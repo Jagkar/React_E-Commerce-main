ba5a40116612072fd73bb5367d436379924b2b8d
Jagdeep Kaur <118887947+Jagkar@users.noreply.github.com>
2024-11-21 10:51:11 +0530

    E commerce Website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            {/* <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
+              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Jagdeep Kaur</a>
+            </p>
+            <a className="text-dark fs-4" href="https://github.com/jagkar" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a> */}
+            &copy; 2024. All rights reserved.
+            <br></br>
+            Shoppers Stop (Pavilion Mall) in Fountain Chowk, Ludhiana. Shoppers Stop, Pavillion Mall, Ludhiana.
+            <br></br>
+            +91 9463061160
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
